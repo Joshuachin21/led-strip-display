344c2f03d003a563a513748545c2a796a6bafd79
DESKTOP-4SP3806\joshu <joshua.chin21@gmail.com>
2021-12-23 20:54:16 -0800

    new lib usage and example

--- led.controller.js
+++ led.controller.js
@@ -1,31 +1,36 @@
-const ws281x = require('rpi-ws281x-native');
-const options = {
-  dma: 10,
-  freq: 800000,
-  gpio: 18,
-  invert: false,
-  brightness: 255,
-  stripType: ws281x.stripType.WS2812
+var ws281x = require('rpi-ws281x');
+
+class Example {
+
+    constructor() {
+        // Current pixel position
+        this.offset = 0;
+
+        // Set my Neopixel configuration
+        this.config = {leds:25};
+
+        // Configure ws281x
+        ws281x.configure(this.config);
+    }
+
+    loop() {
+        var pixels = new Uint32Array(this.config.leds);
+
+        // Set a specific pixel
+        pixels[this.offset] = 0xFF0000;
+
+        // Move on to next
+        this.offset = (this.offset + 1) % this.config.leds;
+
+        // Render to strip
+        ws281x.render(pixels);
+    }
+
+    run() {
+        // Loop every 100 ms
+        setInterval(this.loop.bind(this), 100);
+    }
 };
 
-const channel = ws281x(20, options);
-const colors = channel.array;
-
-// update color-values
-
-const startLights = async (index, delay) => {
-  return new Promise((resolve, reject) => {
-    colors[index] = 0xffcc22;
-    setTimeout(() => {
-      resolve(true);
-    }, delay);
-  });
-};
-
-const startShow = async () => {
-  for (let i = 0; i < 100; i++) {
-    await startLights(i);
-    ws281x.render();
-  }
-};
-startShow();
+var example = new Example();
+example.run();
